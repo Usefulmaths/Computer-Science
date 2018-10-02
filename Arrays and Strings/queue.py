d08c753ddf736e19cef6afa438844ebd31a0a5e5
Liam <kingliam28@ntlworld.com>
2018-10-02 16:08:37 +0100

    Split min and max priority queue into separate classes (inherit)

--- Arrays and Strings/queue.py
+++ Arrays and Strings/queue.py
@@ -12,7 +12,23 @@ class Queue(object):
     def dequeue(self):
         self.queue.delete(0)
 
-    def priority_max_peak(self):
+    def __str__(self):
+        return self.queue.__str__()
+
+
+class MaxPriorityQueue(Queue):
+
+    def __init__(self):
+        super().__init__()
+
+    def enqueue(self, value, priority=-1):
+        self.queue.append(value, priority)
+
+    def dequeue(self):
+        max_priority, max_priority_value, max_index = self.peak()
+        self.queue.delete(max_index)
+
+    def peak(self):
         max_priority = -1E10
         max_priority_value = None
 
@@ -29,7 +45,20 @@ class Queue(object):
 
         return max_priority, max_priority_value, max_index
 
-    def priority_min_peak(self):
+
+class MinPriorityQueue(Queue):
+
+    def __init__(self):
+        super().__init__()
+
+    def enqueue(self, value, priority=-1):
+        self.queue.append(value, priority)
+
+    def dequeue(self):
+        min_priority, min_priority_value, min_index = self.peak()
+        self.queue.delete(min_index)
+
+    def peak(self):
         min_priority = 1E10
         min_priority_value = None
 
@@ -46,29 +75,37 @@ class Queue(object):
 
         return min_priority, min_priority_value, min_index
 
-    def max_priority_dequeue(self):
-        max_priority, max_priority_value, max_index = self.priority_max_peak()
-        self.queue.delete(max_index)
-
-    def min_priority_dequeue(self):
-        min_priority, min_priority_value, min_index = self.priority_min_peak()
-        self.queue.delete(min_index)
-
-    def peak(self):
-        first_element = self.queue.index(0).value
-        return first_element
-
-    def __str__(self):
-        return self.queue.__str__()
 
 if __name__ == '__main__':
+    print('### Standard Queue ###')
     queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    print('Enqueue: ')
+    print(queue)
+    print('Dequeue: ')
+    queue.dequeue()
+    print(queue)
+
+    print('\n### Min Priority Queue ###')
+    queue = MinPriorityQueue()
     queue.enqueue(1, priority=1)
     queue.enqueue(2, priority=10)
-    queue.enqueue(3, priority=5)
+    queue.enqueue(3, priority=0.5)
+    print('Enqueue: ')
+    print(queue)
+    print('Dequeue: ')
+    queue.dequeue()
     print(queue)
 
-    queue.min_priority_dequeue()
+    print('\n### Max Priority Queue ###')
+    queue = MaxPriorityQueue()
+    queue.enqueue(1, priority=1)
+    queue.enqueue(2, priority=10)
+    queue.enqueue(3, priority=0.5)
+    print('Enqueue: ')
     print(queue)
-
-    print(queue.peak())
+    print('Dequeue: ')
+    queue.dequeue()
+    print(queue)
